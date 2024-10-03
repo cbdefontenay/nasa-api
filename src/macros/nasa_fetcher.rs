de27d33203dc484b6d903252739129dcedf70b5f
Cyprien <cbdefontenay@gmail.com>
2024-10-03 20:48:16 +0200

    macro

--- src/macros/nasa_fetcher.rs
+++ src/macros/nasa_fetcher.rs
@@ -1,9 +1,9 @@
 #[macro_export]
 macro_rules! reqwester {
     ($url:expr, $val:expr) => {{
-        let http_to_url = reqwest::Url::parse($url).await.unwrap();
+        let http_to_url = reqwest::Url::parse($url).unwrap();
 
-        let http_response_text = http_to_url.text().await.unwrap_or_else(|_| "No text fetched, sorry...".to_string());
+        let http_response_text = http_to_url.text().unwrap_or_else(|_| "No text fetched, sorry...".to_string());
 
         let extract_text_from_api: Value = serde_json::from_str(&http_response_text).expect("Unable to format JSON.");
 
